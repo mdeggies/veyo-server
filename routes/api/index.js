d4fd057dbb0a1259dee44e5c10b9f23cecbee0c4
Michele Degges <mdeggies@gmail.com>
2016-04-14 16:15:09 -0700

    updates

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,10 +4,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+	console.log('inside router.get /');
 	res.send('in router.get /');
 });
 
 router.post('/', function(req, res, next) {
+	console.log('inside router.post /');
 	res.send('in router.post /');
 });
 
