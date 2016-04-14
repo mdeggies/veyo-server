323eb584eb8dda324d616138a4e05236a944781b
Michele Degges <mdeggies@gmail.com>
2016-04-14 16:49:02 -0700

    updates

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var express = require('express'),
-cors = require('cors'), app = express();
+var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var http = require('http');
 
-app.set('port', process.env.PORT || 3000);
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', require('./routes/api'));
+app.use('/api', require('./routes/api/'));
 
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ', app.get('port'));
 });
 
 app.use(function(req, res, next) {
@@ -30,4 +30,4 @@ app.use(function(req, res, next) {
   console.log('Error:', err);
 });
 
-module.exports = app;
+//module.exports = app;
