fcf0831e7526288e29316cda0b08fb55e72b8635
Michele Degges <mdeggies@gmail.com>
2016-04-19 11:43:52 -0700

    server updates

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,14 +4,43 @@ var fs = require('fs')
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-	console.log('inside router.get /');
-	res.send('in router.get /');
+const FILEPATH = './data/data.json';
+
+router.get('/data', function(req, res, next) {
+	fs.readFile(FILEPATH, function (err, data) {
+		if (err) throw err;
+		res.send(JSON.parse(data));
+	});
 });
 
-router.post('/', function(req, res, next) {
-	console.log('inside router.post /');
-	res.send('in router.post /');
+router.post('/data', function(req, res, next) {
+	var data = req.body;
+	fs.writeFile(FILEPATH, JSON.stringify(data), function (err) {
+		if (err) throw err;
+	});
+	res.end();
+});
+
+router.get('/data/:id', function(req, res, next) {
+	var id = req.params.id-1;
+	fs.readFile(FILEPATH, function (err, data) {
+		if (err) throw err;
+		var data = JSON.parse(data);
+		res.send(data[id]);
+	});
+});
+
+router.delete('/data/:id', function(req, res, next) {
+	var id = req.params.id-1;
+	fs.readFile(FILEPATH, function (err, data) {
+		if (err) throw err;
+		var data = JSON.parse(data);
+		data.splice(id, 1);
+		fs.writeFile(FILEPATH, JSON.stringify(data), function (err) {
+			if (err) throw err;
+		});
+	});
+	res.end();
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
@@ -14,6 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
